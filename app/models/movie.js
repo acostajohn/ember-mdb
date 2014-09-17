ac571cd171e4f17d0594e093b38eb742f8728f2a
John Acosta <johnjaiverac@gmail.com>
2014-09-17 01:19:41 -0500

    Adding pretender

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,17 +1,8 @@
 import DS from 'ember-data';
 
-var Movie = DS.Model.extend({
+export default DS.Model.extend({
   title: DS.attr('string'),
   score: DS.attr('number'),
-  actors: DS.hasMany('actor'),
+  actors: DS.hasMany('actor', { async: true }),
   release: DS.attr('date')
 });
-
-Movie.reopenClass({
-  FIXTURES: [
-    { id: '16521', title: 'First Blood', score: '89', actors: ['1', '2'], release: "2014-05-27T12:54:01" },
-    { id: '14444', title: 'Missing in Action', score: '23', actors: ['3'], release: "2014-05-27T12:54:01" }
-  ]
-});
-
-export default Movie;
