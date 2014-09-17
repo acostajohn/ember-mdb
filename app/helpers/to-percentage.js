8c237488fb3693d1eb5abc99a948d1b3d2035ad6
John Acosta <johnjaiverac@gmail.com>
2014-09-16 23:46:32 -0500

    New helper to handle percentage values

---
+++ app/helpers/to-percentage.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+/**
+ * Converts a numeric value into a percentage value
+ * @param {String|Number} value, Numeric values that might come as a string
+ * @return {String} percentage value
+ */
+function toPercentage(value) {
+  value = Number(value);
+
+  if (isNaN(value)) {
+    value = 0;
+  }
+
+  return value + '%';
+}
+
+export default Ember.Handlebars.makeBoundHelper(toPercentage);
